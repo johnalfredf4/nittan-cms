d17f718148c031e112b2d2e6443f848e22d6a642
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-05 15:10:54 +0800

    Add files via upload

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { UserStatus } from '../../common/enums/user-status.enum';
+import { Role } from '../../roles/entities/role.entity';
+
+@Entity('User_Accounts')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 50 })
+  username: string;
+
+  @Column({ name: 'password_hash', length: 255 })
+  passwordHash: string;
+
+  @Column({ name: 'first_name', length: 100 })
+  firstName: string;
+
+  @Column({ name: 'middle_name', nullable: true, length: 100 })
+  middleName: string;
+
+  @Column({ name: 'last_name', length: 100 })
+  lastName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    default: UserStatus.ACTIVE,
+  })
+  status: UserStatus;
+
+  @ManyToMany(() => Role, (role) => role.users, { eager: true })
+  @JoinTable({
+    name: 'User_Roles',
+    joinColumn: { name: 'user_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
+  })
+  roles: Role[];
+}
