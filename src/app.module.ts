ed7853ed6d72acae8919dffe90b16cf3b2bd207f
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-05 16:24:04 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import ormconfig from './config/ormconfig';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
+import { RolesService } from './roles/roles.service';
 
 @Module({
   imports: [
@@ -18,4 +19,15 @@ import { RolesModule } from './roles/roles.module';
     AuthModule,
   ],
 })
+@Module({
+  imports: [...],
+})
+export class AppModule implements OnModuleInit {
+  constructor(private rolesService: RolesService) {}
+
+  async onModuleInit() {
+    await this.rolesService.seedDefaultRoles();
+    console.log('✔ Roles Seeded');
+  }
+}
 export class AppModule {}
