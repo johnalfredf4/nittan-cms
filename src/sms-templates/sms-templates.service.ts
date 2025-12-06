4944cc62dd73fad355d0b8c86790c7e4ffa1f654
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 14:45:47 +0800

    Update sms-templates.service.ts

--- src/sms-templates/sms-templates.service.ts
+++ src/sms-templates/sms-templates.service.ts
@@ -1,78 +1,48 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { SmsTemplate } from './entities/sms-template.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, In } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
-import { User } from './entities/user.entity';
-import { Role } from '../roles/entities/role.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateSmsTemplateDto } from './dto/create-sms-template.dto';
+import { UpdateSmsTemplateDto } from './dto/update-sms-template.dto';
 
 @Injectable()
-export class UsersService {
+export class SmsTemplatesService {
   constructor(
-    @InjectRepository(User) private readonly usersRepo: Repository<User>,
-    @InjectRepository(Role) private readonly rolesRepo: Repository<Role>,
+    @InjectRepository(SmsTemplate)
+    private smsRepo: Repository<SmsTemplate>,
   ) {}
 
-  async create(dto: CreateUserDto): Promise<User> {
-    const roles = await this.rolesRepo.find({
-      where: { name: In(dto.roleNames) },
-    });
-
-    const passwordHash = await bcrypt.hash(dto.password, 10);
-
-    const user = this.usersRepo.create({
-      username: dto.username,
-      passwordHash,
-      firstName: dto.firstName,
-      middleName: dto.middleName,
-      lastName: dto.lastName,
-      status: dto.status,
-      roles,
-    });
-
-    return this.usersRepo.save(user);
+  async create(dto: CreateSmsTemplateDto) {
+    const template = this.smsRepo.create(dto);
+    return await this.smsRepo.save(template);
   }
 
-  async update(id: number, dto: UpdateUserDto): Promise<User> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
+  async findAll() {
+    return await this.smsRepo.find();
+  }
 
-    if (dto.password) {
-      user.passwordHash = await bcrypt.hash(dto.password, 10);
+  async findOne(id: number) {
+    const template = await this.smsRepo.findOne({ where: { id } });
+    if (!template) {
+      throw new NotFoundException('Template not found');
     }
-    if (dto.firstName !== undefined) user.firstName = dto.firstName;
-    if (dto.middleName !== undefined) user.middleName = dto.middleName;
-    if (dto.lastName !== undefined) user.lastName = dto.lastName;
-    if (dto.status !== undefined) user.status = dto.status;
-
-    if (dto.roleNames) {
-      const roles = await this.rolesRepo.find({
-        where: { name: In(dto.roleNames) },
-      });
-      user.roles = roles;
-    }
-
-    return this.usersRepo.save(user);
+    return template;
   }
 
-  async findByUsername(username: string): Promise<User | null> {
-    return this.usersRepo.findOne({ where: { username } });
+  async update(id: number, dto: UpdateSmsTemplateDto) {
+    const template = await this.findOne(id);
+    Object.assign(template, dto);
+    return await this.smsRepo.save(template);
   }
 
-  async findAll(): Promise<User[]> {
-    return this.usersRepo.find();
+  async remove(id: number) {
+    const template = await this.findOne(id);
+    return await this.smsRepo.remove(template);
   }
 
-  async findOne(id: number): Promise<User> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
-    return user;
-  }
-
-  async remove(id: number): Promise<void> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
-    await this.usersRepo.remove(user);
+  async toggleActive(id: number) {
+    const template = await this.findOne(id);
+    template.isActive = !template.isActive;
+    return await this.smsRepo.save(template);
   }
 }
