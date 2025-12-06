f47b3f04c323ba4dc2cb9954242af65b95457274
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 14:46:33 +0800

    Update sms-templates.module.ts

--- src/sms-templates/sms-templates.module.ts
+++ src/sms-templates/sms-templates.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SmsTemplatesService } from './sms-templates.service';
+import { SmsTemplatesController } from './sms-templates.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { User } from './entities/user.entity';
-import { Role } from '../roles/entities/role.entity';
+import { SmsTemplate } from './entities/sms-template.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
-  controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([SmsTemplate])],
+  controllers: [SmsTemplatesController],
+  providers: [SmsTemplatesService],
 })
-export class UsersModule {}
+export class SmsTemplatesModule {}
