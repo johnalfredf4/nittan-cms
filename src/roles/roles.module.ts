defc83ed9a4d3113b429893b0bcb720668eb4345
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 09:47:25 +0800

    Update roles.module.ts

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Role } from './entities/role.entity';
+import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './role.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
+  controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
 })
