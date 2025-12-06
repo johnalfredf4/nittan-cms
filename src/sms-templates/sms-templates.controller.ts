3836b982d6bbe57762f1d51987283e765acdc92f
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 14:44:16 +0800

    Add files via upload

---
+++ src/sms-templates/sms-templates.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+
+@Controller('users')
+@UseGuards(AuthGuard('jwt')) // applies to all methods
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Roles('IT - CMS Admin', 'Execom - CEO')
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @Roles('IT - CMS Admin')
+  update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  @Roles('IT - CMS Admin')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
