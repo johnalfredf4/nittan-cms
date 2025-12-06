0d5e179ce18b83e5cfc35bbe0e82807465a3bcee
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 14:38:48 +0800

    Update sms-template.entity.ts

--- src/sms-templates/entities/sms-template.entity.ts
+++ src/sms-templates/entities/sms-template.entity.ts
@@ -1,78 +1,25 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, In } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
-import { User } from './entities/user.entity';
-import { Role } from '../roles/entities/role.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Injectable()
-export class UsersService {
-  constructor(
-    @InjectRepository(User) private readonly usersRepo: Repository<User>,
-    @InjectRepository(Role) private readonly rolesRepo: Repository<Role>,
-  ) {}
+@Entity()
+export class SmsTemplate {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-  async create(dto: CreateUserDto): Promise<User> {
-    const roles = await this.rolesRepo.find({
-      where: { name: In(dto.roleNames) },
-    });
+  @Column()
+  templateCode: string;
 
-    const passwordHash = await bcrypt.hash(dto.password, 10);
+  @Column()
+  title: string;
 
-    const user = this.usersRepo.create({
-      username: dto.username,
-      passwordHash,
-      firstName: dto.firstName,
-      middleName: dto.middleName,
-      lastName: dto.lastName,
-      status: dto.status,
-      roles,
-    });
+  @Column({ type: 'text' })
+  message: string;
 
-    return this.usersRepo.save(user);
-  }
+  @Column({ default: true })
+  isActive: boolean;
 
-  async update(id: number, dto: UpdateUserDto): Promise<User> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 
-    if (dto.password) {
-      user.passwordHash = await bcrypt.hash(dto.password, 10);
-    }
-    if (dto.firstName !== undefined) user.firstName = dto.firstName;
-    if (dto.middleName !== undefined) user.middleName = dto.middleName;
-    if (dto.lastName !== undefined) user.lastName = dto.lastName;
-    if (dto.status !== undefined) user.status = dto.status;
-
-    if (dto.roleNames) {
-      const roles = await this.rolesRepo.find({
-        where: { name: In(dto.roleNames) },
-      });
-      user.roles = roles;
-    }
-
-    return this.usersRepo.save(user);
-  }
-
-  async findByUsername(username: string): Promise<User | null> {
-    return this.usersRepo.findOne({ where: { username } });
-  }
-
-  async findAll(): Promise<User[]> {
-    return this.usersRepo.find();
-  }
-
-  async findOne(id: number): Promise<User> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
-    return user;
-  }
-
-  async remove(id: number): Promise<void> {
-    const user = await this.usersRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('User not found');
-    await this.usersRepo.remove(user);
-  }
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 }
