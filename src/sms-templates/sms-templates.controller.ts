e69fc64b51200a49c8b90cd4f4d20d7d85df7908
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 14:46:08 +0800

    Update sms-templates.controller.ts

--- src/sms-templates/sms-templates.controller.ts
+++ src/sms-templates/sms-templates.controller.ts
@@ -2,49 +2,46 @@ import {
   Controller,
   Get,
   Post,
-  Patch,
-  Delete,
   Body,
   Param,
-  UseGuards,
+  Patch,
+  Delete,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from '@nestjs/passport';
-import { RolesGuard } from '../auth/roles.guard';
-import { Roles } from '../auth/roles.decorator';
+import { SmsTemplatesService } from './sms-templates.service';
+import { CreateSmsTemplateDto } from './dto/create-sms-template.dto';
+import { UpdateSmsTemplateDto } from './dto/update-sms-template.dto';
 
-@Controller('users')
-@UseGuards(AuthGuard('jwt')) // applies to all methods
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+@Controller('sms-templates')
+export class SmsTemplatesController {
+  constructor(private readonly service: SmsTemplatesService) {}
 
   @Post()
-  @Roles('IT - CMS Admin', 'Execom - CEO')
-  create(@Body() dto: CreateUserDto) {
-    return this.usersService.create(dto);
+  create(@Body() dto: CreateSmsTemplateDto) {
+    return this.service.create(dto);
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.service.findAll();
   }
-  @UseGuards(AuthGuard('jwt'))
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.service.findOne(+id);
   }
 
   @Patch(':id')
-  @Roles('IT - CMS Admin')
-  update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
-    return this.usersService.update(+id, dto);
+  update(@Param('id') id: string, @Body() dto: UpdateSmsTemplateDto) {
+    return this.service.update(+id, dto);
+  }
+
+  @Patch(':id/toggle-active')
+  toggleActive(@Param('id') id: string) {
+    return this.service.toggleActive(+id);
   }
 
   @Delete(':id')
-  @Roles('IT - CMS Admin')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  delete(@Param('id') id: string) {
+    return this.service.remove(+id);
   }
 }
