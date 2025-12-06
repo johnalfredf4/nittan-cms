3a5adc6674a7efdf69199c1e55c5d19cf19693a2
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 10:46:59 +0800

    Update email-template-form.js

--- src/public/js/email-template-form.js
+++ src/public/js/email-template-form.js
@@ -30,13 +30,19 @@ async function initTemplateForm() {
   document.querySelector("#templateForm").addEventListener("submit", async e => {
     e.preventDefault();
 
+    // Capture WYSIWYG editor content
+    document.getElementById("bodyHidden").value =
+        document.getElementById("bodyInput").innerHTML;
+
     const form = new FormData(e.target);
+    
     const payload = {
       code: form.get("code"),
       name: form.get("name"),
       subject: form.get("subject"),
-      body: form.get("body"),
+      body: form.get("body"), // bodyHidden is used here
     };
+
 
     const url = editId ? `/email-templates/${editId}` : `/email-templates`;
     const method = editId ? "PATCH" : "POST";
