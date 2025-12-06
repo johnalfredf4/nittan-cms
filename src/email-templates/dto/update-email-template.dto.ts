80702db5c632ba0d889a4efa5ecb91a92416e580
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-06 10:14:24 +0800

    Update update-email-template.dto.ts

--- src/email-templates/dto/update-email-template.dto.ts
+++ src/email-templates/dto/update-email-template.dto.ts
@@ -1,34 +1,6 @@
-import {
-  IsString,
-  IsOptional,
-  IsEnum,
-  IsArray,
-} from 'class-validator';
-import { UserStatus } from '../../common/enums/user-status.enum';
-
-export class UpdateUserDto {
-  @IsOptional()
-  @IsString()
-  password?: string;
-
-  @IsOptional()
-  @IsString()
-  firstName?: string;
-
-  @IsOptional()
-  @IsString()
-  middleName?: string;
-
-  @IsOptional()
-  @IsString()
-  lastName?: string;
-
-  @IsOptional()
-  @IsEnum(UserStatus)
-  status?: UserStatus;
-
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
-  roleNames?: string[];
+export class UpdateEmailTemplateDto {
+  name?: string;
+  subject?: string;
+  body?: string;
+  status?: string;
 }
