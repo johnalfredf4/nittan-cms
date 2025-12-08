84b088931b36b8b10ede96ef8c1e31c5d963a2a1
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 14:48:11 +0800

    Add files via upload

---
+++ src/loan-assignment/types/account-class.ts
@@ -0,0 +1,10 @@
+export enum AccountClass {
+  DPD_0 = 'DPD_0',
+  DPD_1_30 = 'DPD_1_30',
+  DPD_31_60 = 'DPD_31_60',
+  DPD_61_90 = 'DPD_61_90',
+  DPD_91_120 = 'DPD_91_120',
+  DPD_121_150 = 'DPD_121_150',
+  DPD_151_180 = 'DPD_151_180',
+  DPD_181_PLUS = 'DPD_181_PLUS',
+}
