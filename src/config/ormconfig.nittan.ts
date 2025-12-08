d567e5149f2cf7a36517da4b39f775d6a7e76be5
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 19:43:27 +0800

    Update ormconfig.nittan.ts

--- src/config/ormconfig.nittan.ts
+++ src/config/ormconfig.nittan.ts
@@ -9,12 +9,12 @@ const nittanConnectionConfig: TypeOrmModuleOptions = {
   password: 'bong3',
   database: 'Nittan',
   synchronize: false,
+  requestTimeout: 6000000,   // 60 seconds for query execution
   entities: [], // ALWAYS EMPTY FOR READ DB
   options: {
     encrypt: false,
     trustServerCertificate: true,
     connectTimeout: 6000000,   // 60 seconds for DB connection
-    requestTimeout: 6000000,   // 60 seconds for query execution
   },
 };
 
