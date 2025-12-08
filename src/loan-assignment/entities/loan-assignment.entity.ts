327c986ed64b52123fd356497094470e6bebde67
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 14:52:14 +0800

    Update loan-assignment.entity.ts

--- src/loan-assignment/entities/loan-assignment.entity.ts
+++ src/loan-assignment/entities/loan-assignment.entity.ts
@@ -6,22 +6,8 @@ import {
   UpdateDateColumn,
   Index,
 } from 'typeorm';
-
-export enum LocationType {
-  HQ = 'HQ',
-  BRANCH = 'BRANCH',
-}
-
-export enum AccountClass {
-  DPD_0 = 'DPD_0',
-  DPD_1_30 = 'DPD_1_30',
-  DPD_31_60 = 'DPD_31_60',
-  DPD_61_90 = 'DPD_61_90',
-  DPD_91_120 = 'DPD_91_120',
-  DPD_121_150 = 'DPD_121_150',
-  DPD_151_180 = 'DPD_151_180',
-  DPD_181_PLUS = 'DPD_181_PLUS',
-}
+import { LocationType } from '../types/location-type';
+import { AccountClass } from '../types/account-class';
 
 @Entity({ name: 'Loan_Assignments' })
 @Index(['loanApplicationId', 'active'], { unique: true })
