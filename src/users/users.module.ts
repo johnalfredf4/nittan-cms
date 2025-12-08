7032c78d3ab1dd44422dd366b6d6b0a8c444cd91
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 15:41:12 +0800

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { Role } from '../roles/entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
+  imports: [
+    TypeOrmModule.forFeature([User, Role], 'nittan_app'),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
+  exports: [UsersService], // optional but recommended
 })
 export class UsersModule {}
