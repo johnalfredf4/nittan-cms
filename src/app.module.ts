d19b9a21465bd91c7fd8d7ed1bd585883fd8adaa
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 12:46:18 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { RolesService } from './roles/roles.service';
 import { AuthModule } from './auth/auth.module';
 import { EmailTemplatesModule } from './email-templates/email-templates.module';
 import { SmsTemplatesModule } from './sms-templates/sms-templates.module';
-import { SmsTemplate } from './sms-templates/entities/sms-template.entity';
 import { ProductTypesModule } from './product-types/product-types.module';
 import { AccountRetentionModule } from './account-retention/account-retention.module';
 import { DispositionsModule } from './dispositions/dispositions.module';
@@ -19,7 +18,26 @@ import { LoanAssignmentModule } from './loan-assignment/loan-assignment.module';
 
 @Module({
   imports: [
+    // ➤ NEW READ-ONLY DATABASE CONNECTION (source DB)
+    TypeOrmModule.forRoot({
+      name: 'nittan',
+      type: 'mssql',
+      host: 'nittan-rds.chsm6icykzm3.ap-southeast-1.rds.amazonaws.com',
+      port: 1433,
+      username: 'bong3',
+      password: 'bong3',
+      database: 'Nittan',
+      synchronize: false,
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+      // ⛔ DO NOT load entities here
+    }),
+
+    // 🔥 Existing CMS database connection (write DB)
     TypeOrmModule.forRoot(ormconfig),
+
     UsersModule,
     RolesModule,
     AuthModule,
@@ -29,6 +47,7 @@ import { LoanAssignmentModule } from './loan-assignment/loan-assignment.module';
     AccountRetentionModule,
     DispositionsModule,
     LoanAssignmentModule,
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'src', 'public'),
     }),
