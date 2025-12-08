652f491450700bbc40c8af6563fce6bbd441f88b
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 15:10:52 +0800

    Update location-constants.ts

--- src/loan-assignment/constants/location-constants.ts
+++ src/loan-assignment/constants/location-constants.ts
@@ -2,4 +2,5 @@ import { LocationType } from '../types/location-type';
 
 export const LOCATION_HQ: LocationType = 'HQ';
 export const LOCATION_BRANCH: LocationType = 'BRANCH';
+
 export const LOCATION_TYPES: LocationType[] = ['HQ', 'BRANCH'];
