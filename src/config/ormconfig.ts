643a1831bbb601202cedf1e089e6a82f9cfdc9bb
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 13:34:03 +0800

    Update ormconfig.ts

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { User } from '../users/entities/user.entity';
 import { Role } from '../roles/entities/role.entity';
 import { EmailTemplate } from '../email-templates/entities/email-template.entity';
@@ -8,17 +9,18 @@ import { ProductType } from '../product-types/entities/product-type.entity';
 import { AccountRetention } from '../account-retention/entities/account-retention.entity';
 import { DispositionCategory } from '../dispositions/entities/disposition-category.entity';
 import { Disposition } from '../dispositions/entities/disposition.entity';
+
 import { LoanAssignment } from '../loan-assignment/entities/loan-assignment.entity';
 import { RotationState } from '../loan-assignment/entities/rotation-state.entity';
 
 const ormconfig: TypeOrmModuleOptions = {
-  name: 'nittan_app',  // ⭐ REQUIRED
+  // ✅ NO `name` → this is the DEFAULT connection
   type: 'mssql',
   host: 'nittan-rds.chsm6icykzm3.ap-southeast-1.rds.amazonaws.com',
   port: 1433,
   username: 'bong3',
   password: 'bong3',
-  database: 'Nittan-App',
+  database: 'Nittan_App',
   entities: [
     User,
     Role,
