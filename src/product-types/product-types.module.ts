3ed607bfdcff9cd99d60301647a440eeae1cceee
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 15:43:23 +0800

    Update product-types.module.ts

--- src/product-types/product-types.module.ts
+++ src/product-types/product-types.module.ts
@@ -5,8 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductType } from './entities/product-type.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductType])],
+  imports: [
+    TypeOrmModule.forFeature([ProductType], 'nittan_app'), // 👈 IMPORTANT FIX
+  ],
   controllers: [ProductTypesController],
   providers: [ProductTypesService],
+  exports: [ProductTypesService], // 👈 recommended to export
 })
 export class ProductTypesModule {}
