4d1a4264e1ebf24224d21c8a4a6e2809fe2e6b76
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-08 16:02:42 +0800

    Update ormconfig.ts

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -9,7 +9,6 @@ import { ProductType } from '../product-types/entities/product-type.entity';
 import { AccountRetention } from '../account-retention/entities/account-retention.entity';
 import { DispositionCategory } from '../dispositions/entities/disposition-category.entity';
 import { Disposition } from '../dispositions/entities/disposition.entity';
-
 import { LoanAssignment } from '../loan-assignment/entities/loan-assignment.entity';
 import { RotationState } from '../loan-assignment/entities/rotation-state.entity';
 
