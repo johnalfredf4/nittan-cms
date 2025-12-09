0f3e785762407fb8d148f20326882193bad731b4
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-10 01:42:29 +0800

    Update loanreceivable-assignment.controller.ts

--- src/loanreceivable-assignment/loanreceivable-assignment.controller.ts
+++ src/loanreceivable-assignment/loanreceivable-assignment.controller.ts
@@ -11,7 +11,7 @@ import { BulkOverrideAssignmentDto } from './dto/bulk-override.dto';
 
 @Controller('loanreceivable-assignment')
 export class LoanreceivableAssignmentController {
-  constructor(private readonly service: LoanreceivableAssignmentService) {}
+  constructor(private readonly service: LoanReceivableAssignmentService) {}
 
   @Post('bulk-override')
   async bulkOverride(@Body() dto: BulkOverrideAssignmentDto) {
