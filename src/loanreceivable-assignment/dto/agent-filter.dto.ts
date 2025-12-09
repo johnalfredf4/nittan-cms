a03185a8d61f3cb752f258fcac98ec9f3d3857b9
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-10 01:45:13 +0800

    Update agent-filter.dto.ts

--- src/loanreceivable-assignment/dto/agent-filter.dto.ts
+++ src/loanreceivable-assignment/dto/agent-filter.dto.ts
@@ -1,16 +1,7 @@
-import { IsOptional, IsNumber, IsString, IsIn } from 'class-validator';
+import { IsOptional, IsInt } from 'class-validator';
 
 export class AgentFilterDto {
   @IsOptional()
-  @IsNumber()
-  branchId?: number;
-
-  @IsOptional()
-  @IsString()
-  @IsIn(['HQ', 'BRANCH'])
-  location?: 'HQ' | 'BRANCH';
-
-  @IsOptional()
-  @IsString()
-  roleName?: string;
+  @IsInt()
+  agentId?: number;
 }
