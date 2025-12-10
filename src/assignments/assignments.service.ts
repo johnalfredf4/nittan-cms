2f29c8e725db1017425baee6256010485b74c7c7
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-10 18:31:31 +0800

    Update assignments.service.ts

--- src/assignments/assignments.service.ts
+++ src/assignments/assignments.service.ts
@@ -37,4 +37,34 @@ export class AssignmentsService {
 
     return result;
   }
+
+  async getByLoanReceivableId(loanReceivableId: number) {
+  const result = await this.reportingConnection.query(
+    `
+      SELECT la.[loanApplicationId]
+            ,la.[loanReceivableId]
+            ,la.[agentId]
+            ,la.[branchId]
+            ,la.[locationType]
+            ,la.[dpd]
+            ,la.[dpdCategory]
+            ,la.[retentionDays]
+            ,la.[retentionUntil]
+            ,la.[status]
+            ,la.[createdAt]
+            ,la.[updatedAt]
+            ,lr.DueDate
+            ,lr.Balance
+            ,l.*
+      FROM [Nittan-App].[dbo].[LoanReceivable_Assignments] AS la
+      INNER JOIN Nittan.dbo.tblLoanReceivables AS lr ON lr.ID = la.loanReceivableId
+      INNER JOIN Nittan.dbo.tblLoanApplications AS l ON l.ID = la.loanApplicationId
+      WHERE la.[loanReceivableId] = @0
+    `,
+    [loanReceivableId],
+  );
+
+  return result;
+}
+
 }
