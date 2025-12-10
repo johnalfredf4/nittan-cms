e87a5bbff6b6ce159a67634ec660d850ceae8cc9
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-10 16:27:47 +0800

    Update assignments.module.ts

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AssignmentsService } from './assignments.service';
 import { AssignmentsController } from './assignments.controller';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [JwtModule], // allow token validation
-  providers: [AssignmentsService],
+  imports: [
+    AuthModule, // JWT validation
+    TypeOrmModule.forFeature([], 'nittan_app'), // tells Nest we use this DB
+  ],
   controllers: [AssignmentsController],
+  providers: [AssignmentsService],
 })
 export class AssignmentsModule {}
