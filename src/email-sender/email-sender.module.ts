38ad5b6d157c68218a62bd7a8d5eac8bc8908c6c
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-15 19:29:28 +0800

    Update email-sender.module.ts

--- src/email-sender/email-sender.module.ts
+++ src/email-sender/email-sender.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailSenderController } from './email-sender.controller';
 import { EmailSenderService } from './email-sender.service';
+import { EmailSendLog } from './entities/email-send-log.entity';
 
 @Module({
+   imports: [
+    TypeOrmModule.forFeature([EmailSendLog], 'nittan_app'),
+  ],
   controllers: [EmailSenderController],
   providers: [EmailSenderService],
 })
