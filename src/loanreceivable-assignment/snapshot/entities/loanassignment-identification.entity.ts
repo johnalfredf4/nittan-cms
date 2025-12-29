a016f908aeb56e4b87147eb28e02807268034d37
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-29 19:10:28 +0800

    Update loanassignment-identification.entity.ts

--- src/loanreceivable-assignment/snapshot/entities/loanassignment-identification.entity.ts
+++ src/loanreceivable-assignment/snapshot/entities/loanassignment-identification.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { LoanAssignmentPersonalSnapshot } from './loanassignment-personal-snapshot.entity';
 
 @Entity('LoanAssignment_Identifications')
@@ -6,18 +12,23 @@ export class LoanAssignmentIdentification {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => LoanAssignmentPersonalSnapshot, s => s.identifications)
+  @ManyToOne(
+    () => LoanAssignmentPersonalSnapshot,
+    snapshot => snapshot.identifications,
+    { onDelete: 'CASCADE' },
+  )
+  @JoinColumn({ name: 'personalSnapshotId' }) // 👈 must exist in DB
   snapshot: LoanAssignmentPersonalSnapshot;
 
-  @Column()
+  @Column({ length: 50 })
   idType: string;
 
-  @Column()
+  @Column({ length: 100 })
   idNumber: string;
 
   @Column({ type: 'date', nullable: true })
   dateIssued?: Date;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   countryIssued?: string;
 }
