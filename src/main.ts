4c10ec13dd8c2fa8692e0698cb759a8fde7baf53
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2025-12-30 08:19:56 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,16 @@ import { join } from 'path';
 import * as express from 'express';
 
 async function bootstrap() {
-  // 🔑 IMPORTANT: type the app as NestExpressApplication
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Allow frontend origin
   app.enableCors({
     origin: ['http://localhost:5173', 'http://10.254.0.3'],
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
 
-  // Serve static files (Tailwind CSS)
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
-    cacheControl: false, // dev-friendly
-  });
+  // ✅ Serve static files (Tailwind CSS)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   // Body parsing
   app.use(express.json());
