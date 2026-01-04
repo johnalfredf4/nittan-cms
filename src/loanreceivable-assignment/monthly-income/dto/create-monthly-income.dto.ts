efbcdcd688af942ecf9bd710f6b53ba39d25fe3f
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2026-01-04 15:26:19 +0800

    Update create-monthly-income.dto.ts

--- src/loanreceivable-assignment/monthly-income/dto/create-monthly-income.dto.ts
+++ src/loanreceivable-assignment/monthly-income/dto/create-monthly-income.dto.ts
@@ -1,31 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateMonthlyIncomeDto {
-    @ApiProperty()
-    @IsNumber()
-    personalSnapshotId: number;
-
-    @ApiProperty()
-    @IsString()
-    incomeType: string;
-
-    @ApiProperty()
-    @IsNumber()
-    amount: number;
-
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsString()
-    bankName?: string;
-
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsString()
-    bankBranch?: string;
-
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsString()
-    accountNumber?: string;
+  personalSnapshotId: number;
+  incomeType: string;
+  amount: number;
+  bankName?: string;
+  bankBranch?: string;
+  accountNumber?: string;
 }
+
+
