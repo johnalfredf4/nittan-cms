8b4cc76f6a7a276e44cc4942b55c885c5523cde9
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2026-01-04 15:33:43 +0800

    Update update-monthly-income.dto.ts

--- src/loanreceivable-assignment/monthly-income/dto/update-monthly-income.dto.ts
+++ src/loanreceivable-assignment/monthly-income/dto/update-monthly-income.dto.ts
@@ -2,9 +2,10 @@ import { CreateMonthlyIncomeDto } from './create-monthly-income.dto';
 
 export class UpdateMonthlyIncomeDto {
   incomeType?: string;
-  amount?: number;
+  amount?: string;
   bankName?: string;
   bankBranch?: string;
   accountNumber?: string;
 }
+
 
