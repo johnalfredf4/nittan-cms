49b2dd4167d3b64b06bdad42a7bd2f4bdeb9ba7c
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2026-01-15 20:07:40 +0800

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,14 +4,16 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { Role } from '../roles/entities/role.entity';
+import { Branch } from '../branches/entities/branch.entity';
 import { BranchesModule } from '../branches/branches.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Role, BranchesModule], 'nittan_app'),
+    TypeOrmModule.forFeature([User, Role, Branch], 'nittan_app'),
+    BranchesModule, // ✅ modules go here, not in forFeature
   ],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService], // optional but recommended
+  exports: [UsersService],
 })
 export class UsersModule {}
