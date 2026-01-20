48a9646312238d45a118ca93d8c0147b1662bb1a
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2026-01-20 19:04:34 +0800

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,11 +6,13 @@ import { User } from './entities/user.entity';
 import { Role } from '../roles/entities/role.entity';
 import { Branch } from '../branches/entities/branch.entity';
 import { BranchesModule } from '../branches/branches.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Role, Branch], 'nittan_app'),
     BranchesModule, // ✅ modules go here, not in forFeature
+    HttpModule, // ✅ ADD
   ],
   controllers: [UsersController],
   providers: [UsersService],
