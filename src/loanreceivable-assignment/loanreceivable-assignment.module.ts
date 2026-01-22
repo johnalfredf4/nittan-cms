3a47b3e72eca080c4b862388e71a8b19ab93f66e
johnalfredf4 <58462396+johnalfredf4@users.noreply.github.com>
2026-01-22 19:30:35 +0800

    Update loanreceivable-assignment.module.ts

--- src/loanreceivable-assignment/loanreceivable-assignment.module.ts
+++ src/loanreceivable-assignment/loanreceivable-assignment.module.ts
@@ -1,6 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+//import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+
+import { AgentWorkloadModule } from './agent-workload/agent-workload.module';
 /* =======================
    Assignment Core
 ======================= */
@@ -79,7 +82,7 @@ import { AgentWorkloadModule } from './agent-workload/agent-workload.module';
     ============================================ */
     DocumentsModule,
     CallRecordingsModule,
-     
+    forwardRef(() => AgentWorkloadModule), // ✅ MIRROR
   ],
 
   controllers: [
